f0f60fe064ed9b4ccf225f487ac2a7a0d1e58c6d
Jeff <rustyangel@gmail.com>
2025-07-25 22:23:09 -0600

    Simplify grammar definition

--- grammar.js
+++ grammar.js
@@ -14,37 +14,174 @@ const { rules: statementRules } = require('./grammar-statements.js')
 module.exports = grammar({
 	name: 'nunjucks',
 
-	extras: (_) => [],
+	extras: (_) => [/\s/],
 	externals: ($) => [$.raw_start, $._raw_char, $.raw_end, $._inline_words],
 
 	rules: {
-		source: ($) =>
-			repeat(
-				choice($.statement, $.expression, $.content, $.raw_block, $.comment),
-			),
+		template: ($) =>
+			repeat(choice($.expression_tag, $.statement_tag, $.content)),
 
-		...literalRules,
-		...expressionRules,
-		...statementRules,
-
-		expression: ($) =>
+		expression_tag: ($) =>
 			seq(
 				choice('{{', '{{-', '{{+'),
-				optional(seq($._expression, optional($.ternary_expression))),
+				optional(seq($.expression, optional($.ternary_expression))),
 				choice('}}', '-}}', '+}}'),
 			),
 
-		statement: ($) =>
-			seq(choice('{%', '{%-', '{%+'), $._statement, choice('-%}', '%}', '+%}')),
-
-		raw_block: ($) =>
-			seq($.raw_start, alias(repeat($._raw_char), $.raw_body), $.raw_end),
-
-		comment: (_) =>
-			choice(
-				// Is this for inline jinja? Might not be needed.
-				seq('##', /[^\r\n]*/, /\r?\n/),
-				seq('{#', repeat(choice(/[^#]+/, '#')), '#}'),
+		statement_tag: ($) =>
+			seq(
+				choice('{%', '{%-', '{%+'),
+				optional($.statement),
+				choice('-%}', '%}', '+%}'),
 			),
+
+		/**
+		 * Expressions.
+		 */
+
+		expression: ($) =>
+			prec.left(
+				choice(
+					$.unary_expression,
+					seq($.expression, $.binary_operator, $.unary_expression),
+					seq($.expression, '.', $.expression),
+					seq($.expression, '[', $.expression, ']'),
+				),
+			),
+
+		ternary_expression: ($) =>
+			seq('if', $.expression, optional(seq('else', $.expression))),
+
+		binary_operator: (_) =>
+			choice(
+				'+',
+				'-',
+				'*',
+				'/',
+				'//',
+				'%',
+				'**',
+				'==',
+				'===',
+				'!=',
+				'!==',
+				'>',
+				'>=',
+				'<',
+				'<=',
+				'and',
+				'or',
+				// "|",
+				// "~",
+			),
+
+		unary_expression: ($) =>
+			choice($.primary_expression, seq($.unary_operator, $.unary_expression)),
+		unary_operator: (_) => choice('not', '!'),
+
+		primary_expression: ($) =>
+			choice(
+				$.function_call,
+				$.literal,
+				$.identifier,
+				seq('(', $.expression, ')'),
+			),
+
+		/** @todo Should the args be its own type? */
+		function_call: ($) =>
+			seq(
+				$.identifier,
+				'(',
+				separated(seq($.expression, optional($.ternary_expression))),
+				')',
+			),
+
+		/**
+		 * Statements.
+		 */
+
+		statement: ($) => choice($.if_statement, $.for_statement, $.end_statement),
+
+		if_statement: ($) => seq('if', $.expression),
+
+		for_statement: ($) =>
+			seq('for', separated1($.identifier), 'in', $.expression),
+
+		end_statement: ($) => choice('endif', 'endfor'),
+
+		/**
+		 * Literals.
+		 */
+
+		literal: ($) =>
+			choice(
+				$.boolean_literal,
+				$.null_literal,
+				$.number_literal,
+				$.float_literal,
+				$.list_literal,
+				// $.tuple_literal,
+				$.string_literal,
+				$.dict_literal,
+			),
+		boolean_literal: (_) => choice('true', 'false'),
+		null_literal: (_) => choice('null', 'none'),
+		number_literal: ($) =>
+			seq(
+				optional(choice('-', '+')),
+				choice(
+					$.dec_literal,
+					$.oct_literal,
+					$.bin_literal,
+					$.hex_literal,
+					$.big_dec_literal,
+				),
+			),
+		dec_literal: (_) => /\d[\d_]*/,
+		big_dec_literal: ($) =>
+			seq($.dec_literal, choice('e', 'e+', 'e-'), optional('_'), $.dec_literal),
+		oct_literal: (_) => /0[oO][0-7_]+/,
+		bin_literal: (_) => /0[bB][01_]+/,
+		hex_literal: (_) => /0[xX][\da-fA-F_]+/,
+		float_literal: ($) => prec(2, seq($.dec_literal, '.', $.dec_literal)),
+
+		list_literal: ($) => seq('[', separated($.expression), optional(','), ']'),
+		// tuple_literal: ($) =>
+		//   prec(1, seq("(", separated($.expression), optional(","), ")")),
+		dict_literal: ($) =>
+			seq('{', separated(seq($.expression, ':', $.expression)), '}'),
+
+		string_literal: (_) =>
+			choice(
+				seq('"', repeat(choice(/[^"]/, '\\"')), '"'),
+				seq("'", repeat(choice(/[^']/, "\\'")), "'"),
+			),
+
+		/**
+		 * Generic types.
+		 */
+
+		content: (_) =>
+			prec.right(repeat1(choice(/[^\{]/, /\{[^\#\%]/, '#', '# '))),
+
+		identifier: (_) => /[a-zA-Z_][\w\d_]*/,
 	},
 })
+
+/**
+ * @param {Rule} rule
+ * @param {string} sep
+ * @returns {SeqRule}
+ */
+function separated1(rule, sep = ',') {
+	return seq(rule, repeat(seq(sep, rule)))
+}
+
+/**
+ * @param {Rule} rule
+ * @param {string} sep
+ * @returns {ChoiceRule}
+ */
+function separated(rule, sep = ',') {
+	return optional(separated1(rule, sep))
+}
