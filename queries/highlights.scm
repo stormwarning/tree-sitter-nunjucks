0c82e0890fd1f95f28434b3087b4e5834046d090
Bob Rockefeller <bob@bobrockefeller.com>
2026-02-21 15:59:02 -0500

    Added highlights.scm

---
+++ queries/highlights.scm
@@ -0,0 +1,151 @@
+; ---------------------------------------------------------------------------
+; Tag delimiters  →  punctuation.bracket
+; ---------------------------------------------------------------------------
+
+"{{" @punctuation.bracket
+"{{-" @punctuation.bracket
+"{{+" @punctuation.bracket
+"}}" @punctuation.bracket
+"-}}" @punctuation.bracket
+"+}}" @punctuation.bracket
+
+"{%" @punctuation.bracket
+"{%-" @punctuation.bracket
+"{%+" @punctuation.bracket
+"%}" @punctuation.bracket
+"-%}" @punctuation.bracket
+"+%}" @punctuation.bracket
+
+"{#" @punctuation.bracket
+"#}" @punctuation.bracket
+
+; ---------------------------------------------------------------------------
+; Comments
+; ---------------------------------------------------------------------------
+
+(comment_tag) @comment
+
+; ---------------------------------------------------------------------------
+; Keywords
+; ---------------------------------------------------------------------------
+
+[
+  "if"
+  "elif"
+  "elseif"
+  "else"
+  "endif"
+] @keyword.conditional
+
+[
+  "for"
+  "in"
+  "endfor"
+  "asyncEach"
+  "endeach"
+  "asyncAll"
+  "endall"
+] @keyword.repeat
+
+[
+  "macro"
+  "endmacro"
+  "call"
+  "endcall"
+] @keyword.function
+
+[
+  "set"
+  "endset"
+  "block"
+  "endblock"
+  "extends"
+  "include"
+  "import"
+  "from"
+  "as"
+  "filter"
+  "endfilter"
+  "switch"
+  "endswitch"
+  "case"
+  "default"
+  "raw"
+  "endraw"
+  "verbatim"
+  "endverbatim"
+] @keyword
+
+[
+  "ignore"
+  "missing"
+  "with"
+  "without"
+  "context"
+] @keyword.modifier
+
+; ---------------------------------------------------------------------------
+; Operators
+; ---------------------------------------------------------------------------
+
+(binary_operator) @operator
+(unary_operator) @operator
+
+"." @punctuation.delimiter
+":" @punctuation.delimiter
+"|" @operator
+"=" @operator
+
+; ---------------------------------------------------------------------------
+; Punctuation
+; ---------------------------------------------------------------------------
+
+"(" @punctuation.bracket
+")" @punctuation.bracket
+"[" @punctuation.bracket
+"]" @punctuation.bracket
+"{" @punctuation.bracket
+"}" @punctuation.bracket
+
+"," @punctuation.delimiter
+
+; ---------------------------------------------------------------------------
+; Literals
+; ---------------------------------------------------------------------------
+
+(boolean_literal) @boolean
+
+(null_literal) @constant.builtin
+
+(number_literal) @number
+(float_literal) @number.float
+
+(string_literal) @string
+
+; ---------------------------------------------------------------------------
+; Functions
+; ---------------------------------------------------------------------------
+
+(function_call
+  (identifier) @function.call)
+
+(expression_filter
+  (function_call
+    (identifier) @function.call))
+
+(expression_filter
+  (identifier) @function.call)
+
+(macro_statement
+  (function_call
+    (identifier) @function.definition))
+
+(call_statement
+  (function_call
+    (identifier) @function.call))
+
+; ---------------------------------------------------------------------------
+; Identifiers
+; ---------------------------------------------------------------------------
+
+(identifier) @variable
