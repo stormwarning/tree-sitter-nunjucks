ff12df1280bb6aa6b6e6afbefa9bc4d1093ad3a3
Bob Rockefeller <bob@bobrockefeller.com>
2026-02-21 17:54:41 -0500

    Added JS front matter injection

--- queries/injections.scm
+++ queries/injections.scm
@@ -3,4 +3,8 @@
 	(#set! injection.combined))
 
 ((front_matter) @injection.content
+	(#match? @injection.content "\\A---(javascript|js)\\b")
+	(#set! injection.language "javascript"))
+
+((front_matter) @injection.content
 	(#set! injection.language "yaml"))
